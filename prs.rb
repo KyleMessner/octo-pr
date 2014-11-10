2821fc023e72330b11c2c266333d787cb6807ba3
chrissalij <chris@salij.org>
2014-11-09 22:42:24 -0800

    Refactor the code to be more object oriented to make it a bit easier to test.

--- prs.rb
+++ prs.rb
@@ -18,7 +18,240 @@
   end
 end
 
-config_file = '~/prs.yml'
+# ----------------------------------------------------------------------------#
+# BEGIN: Helper Functions
+# ----------------------------------------------------------------------------#
+
+# Tuple to help with validation of required values in the yaml config file.
+# Takes the name of the value, and the expected type.
+Validator = Struct.new(:name, :type)
+
+# Outputs a message to the terminal that can be replaced by calling this
+# method again.
+# To empty the line call this method with an empty string as a message.
+#
+# NOTE: Throws an error if the line is longer than ther terminal window.
+def buffer(message);
+  buffer = Integer(`tput co`) - message.size
+  print "\r#{message}#{" " * (buffer)}"
+  $stdout.flush
+end
+
+# Class which handles opening urls in the default browser.
+class Browser
+
+  def initialize;
+    @already_opened_prs = Set.new()
+  end
+
+  # Open a url in the browser.
+  # Automatically filters urls if they've already been opened.
+  def open(url);
+    if !@already_opened_prs.include? url
+      # Because Launchy sometimes fails to open multiple tabs if it is called too quickly
+      sleep(0.1)
+      Launchy.open(url) do |exception|
+        puts "Attempted to open #{url} and failed because #{exception}"
+      end
+      @already_opened_prs.add url
+    else
+      puts "filtered #{url}"
+    end
+  end
+
+end
+
+# Holds the general configuration for the app
+class Configuration
+
+  attr_accessor :username, :password, :auth_token, :org, :repos, :authors, :auto_open, :show_link, :indent, :verbose_mode, :quiet_mode, :seperator
+
+  # Initialize the config. Takes two parameters;
+  # options - The command line options
+  # yaml - The configuration yaml file with default values
+  def initialize(options, yaml);
+    validate(yaml)
+
+    @username = options[:username] ||= yaml['github']['username']
+    @password = options[:password] ||= yaml['github']['password']
+    @auth_token = options[:auth_token] ||= yaml['github']['auth_token']
+
+    @org = options[:org] ||= yaml['github']['org']
+    @repos = (options[:repos] ||= yaml['github']['repos']).sort
+    @authors = (options[:authors] ||= yaml['github']['authors']).sort.map { |author| author.downcase.strip }
+    @auto_open = options[:auto_open] ||= yaml['github']['auto_open'] ||= false
+    @show_link = options[:show_link] ||= yaml['github']['show_link'] ||= false
+    @indent = options[:indent] ||= yaml['github']['indent'] ||= "    "
+
+    @verbose_mode = options[:verbose_mode] ||= yaml['github']['verbose_mode'] ||= false
+
+    # If verbose mode is set from config file or via the command line, override quiet mode
+    @quiet_mode = !options[:verbose_mode] ||= !yaml['github']['verbose_mode'] ||= options[:quiet_mode] ||= yaml['github']['quiet_mode'] ||= false
+
+    # The string used to separate blocks of output
+    @seperator = '--------------------------'
+
+  end
+
+  def update_repos(repos);
+    @repos = repos
+  end
+
+  def print;
+    if @verbose_mode
+      puts @seperator
+      if @auth_token.nil?
+        puts "Using Auth: username/password '#{@username}'/'#{"*" * @password.size}'"
+      else
+        puts "Using Auth: Token '#{"*" * @auth_token.size}'"
+      end
+      if @auto_open
+        puts "Automatically opening PRs made by;"
+      else
+        puts "Finding open PRs made by;"
+      end
+      puts "#{@indent}* #{@authors.join("\n#{@indent}* ")}"
+      puts "In the organization '#{@org}' to the following repos;"
+      puts "#{@indent}* #{@repos.join("\n#{@indent}* ")}"
+
+      puts "Formatting;"
+      if @show_link
+        puts "#{@indent}Printing links for each PR."
+      else
+        puts "#{@indent}Not printing links for each PR."
+      end
+      puts "#{@indent}And using an indent of '#{@indent}'."
+      puts @seperator
+    end
+  end
+
+  private
+
+  # Validate that all the required config values are present
+  def validate(yaml);
+    [
+      Validator.new('org', String),
+      Validator.new('repos', Array),
+      Validator.new('authors', Array)
+    ].each do |tuple|
+      param = yaml['github'][tuple.name]
+      raise "Missing requred config param '#{tuple.name}'" unless !param.nil?
+      raise "Param '#{tuple.name}' is of the wrong type. Expected #{tuple.type}, was actually #{param.class.name}" unless param.is_a? tuple.type
+    end
+
+    if !((yaml['github']['username'] && yaml['github']['password']) || yaml['github']['auth_token'])
+      raise "You must specify either a username/password combo or an auth token to use."
+    end
+  end
+
+end
+
+# Holds found PRs and provides some handy accessors
+class PRs
+
+  attr_accessor :author_map, :number_lookup
+
+  def initialize;
+    # A map of author name -> list of PRs (title, number, url)
+    @author_map = Hash.new(Hash.new([]))
+    # A map of number to -> url (for each lookup)
+    @number_lookup = Hash.new([])
+  end
+
+  # Add a given pr to the list of prs
+  def add(author, repo, title, number, url);
+    number = number.to_s
+
+    if !@author_map.has_key? author
+      @author_map[author] = Hash.new()
+    end
+
+    if !@author_map[author].has_key? repo
+      @author_map[author][repo] = Array.new()
+    end
+
+    @author_map[author][repo].push({ 'title' => title, 'number' => number, 'url' => url })
+    @number_lookup["#{repo}:#{number}"] = url
+  end
+
+  # Get all the authors with an open pr
+  def authors;
+    @author_map.keys.sort
+  end
+
+  # Get all the open pr numbers
+  def numbers;
+    @number_lookup.keys.sort
+  end
+
+  # Get all the urls for a given lookup.
+  # Essentially just an easy shorthand for looking up by number + author.
+  def urls_for(lookup);
+    urls_by_number(lookup) + urls_by_author(lookup)
+  end
+
+  def urls_by_number(number);
+    @number_lookup[number]
+  end
+
+  def urls_by_author(author);
+    @author_map[author].flat_map do |repos,commits|
+      commits.map do |commit|
+        commit['url']
+      end
+    end
+  end
+
+  def get_all_urls;
+    @author_map.flat_map do |_,repo|
+      repo.flat_map do |_,commits|
+        commits.map do |commit|
+          commit['url']
+        end
+      end
+    end
+  end
+
+  # Prints a status of all prs found.
+  # config - The configuration file. Used for indentation, and determining
+  #          the list of expected authors.
+  def print_status(config);
+    @author_map.each do |author, repo|
+      puts "#{author}"
+      repo.each do |repo, commits|
+        puts "#{config.indent}#{commits.size} PR#{(commits.size == 1) ? "" : "s" } against #{repo}"
+
+        if !config.quiet_mode
+          # We're not in quiet mode, print out the title
+          commits.each do |commit|
+            puts "#{config.indent * 2}#{commit['number']}: #{commit['title']}"
+
+            # And if they want the link printed out, do that on the next line
+            if config.show_link
+              puts "#{config.indent * 3}#{commit['url']}"
+            end
+          end
+
+        elsif config.show_link
+          # We're in quiet mode, but they've said they want to see the links
+          commits.each do |commit|
+            puts "#{config.indent * 2}#{commit['url']}"
+          end
+        end
+      end
+    end
+
+    if !config.quiet_mode
+      noPrs = (config.authors - authors)
+      puts "NOTE: #{noPrs.join(", ")} #{(noPrs.size == 1) ? "has" : "have"} no open prs"
+    end
+  end
+
+end
+
+# ----------------------------------------------------------------------------#
+# END: Helper Functions
+# ----------------------------------------------------------------------------#
 
 # ----------------------------------------------------------------------------#
 # BEGIN: Configuration
@@ -26,7 +259,7 @@ config_file = '~/prs.yml'
 
 options = {}
 OptionParser.new do |opts|
-  opts.banner = "Usage: #{__FILE__} [options]"
+  opts.banner = "Usage: `#{__FILE__} -c /path/to/file.yml [options]`"
   options[:opts] = opts
   
   # Misc config
@@ -91,7 +324,6 @@ OptionParser.new do |opts|
     options[:auth_token] = auth_token
   end
   
-  
 end.parse!
 
 # Just in case someone types `<script> help`.
@@ -101,143 +333,48 @@ if ARGV[0] == "help"
 end
 
 # Load the config file
-config_file = options[:config] || config_file
-if File.exists? config_file
-  puts "Using config file at '#{config_file}'"
+if !options.has_key? :config
+  puts "No config file specified. You must specify a yaml config file."
+  puts options[:opts]
+  exit
+end
+config_path = options[:config]
+if File.exists? config_path
+  puts "Using config file at '#{config_path}'"
 else
-  puts "Woah, config file '#{config_file}' doesn't exist, can't continue."
+  puts "Woah, config file '#{config_path}' doesn't exist, can't continue."
   puts "Exiting immediately."
   exit
 end
-config = YAML.load(File.open(config_file))
+yaml = YAML.load(File.open(config_path))
 
-# Validate that all the required config values are present
-Validator = Struct.new(:name, :type)
-[
-  Validator.new('org', String),
-  Validator.new('repos', Array),
-  Validator.new('authors', Array)
-].each do |tuple|
-  conf = config['github'][tuple.name]
-  raise "Missing requred config param '#{tuple.name}'" unless !conf.nil?
-  raise "Param '#{tuple.name}' is of the wrong type. Expected #{tuple.type}, was actually #{conf.class.name}" unless conf.is_a? tuple.type
-end
-
-if !((config['github']['username'] && config['github']['password']) || config['github']['auth_token'])
-  raise "You must specify either a username/password combo or an auth token to use."
-end
-
-# Set all variables as needed. If they haven't been overwritten 
-username = options[:username] ||= config['github']['username']
-password = options[:password] ||= config['github']['password']
-auth_token = options[:auth_token] ||= config['github']['auth_token']
-
-org = options[:org] ||= config['github']['org']
-repos = (options[:repos] ||= config['github']['repos']).sort
-authors = (options[:authors] ||= config['github']['authors']).sort.map { |author| author.downcase.strip }
-auto_open = options[:auto_open] ||= config['github']['auto_open'] ||= false
-show_link = options[:show_link] ||= config['github']['show_link'] ||= false
-indent = options[:indent] ||= config['github']['indent'] ||= "    "
-
-verbose_mode = options[:verbose_mode] ||= config['github']['verbose_mode'] ||= false
-# If verbose mode is set from config file or via the command line, override quiet mode
-quiet_mode = !options[:verbose_mode] ||= !config['github']['verbose_mode'] ||= options[:quiet_mode] ||= config['github']['quiet_mode'] ||= false
-
-# The string used to separate blocks of output
-seperator = '--------------------------'
+config = Configuration.new(options, yaml)
 
 # ----------------------------------------------------------------------------#
 # END: Configuration
 # ----------------------------------------------------------------------------#
 
 # ----------------------------------------------------------------------------#
-# BEGIN: Helper Functions
-# ----------------------------------------------------------------------------#
-
-# Outputs a message to the terminal that can be replaced by calling this
-# method again.
-# To empty the line call this method with an empty string as a message.
-#
-# NOTE: Throws an error if the line is longer than ther terminal window.
-def buffer(message);
-  buffer = Integer(`tput co`) - message.size
-  print "\r#{message}#{" " * (buffer)}"
-  $stdout.flush
-end
-
-# Class which handles opening urls in the default browser.
-class Browser
-
-  @@already_opened_prs = Set.new()
-
-  # Open a url in the browser.
-  # Automatically filters urls if they've already been opened.
-  def open(url);
-    if !@@already_opened_prs.include? url
-      # Because Launchy sometimes fails to open multiple tabs if it is called too quickly
-      sleep(0.1)
-      Launchy.open(url) do |exception|
-        puts "Attempted to open #{uri} and failed because #{exception}"
-      end
-      @@already_opened_prs.add url
-    else
-      puts "filtered #{url}"
-    end
-  end
-
-end
-
-# ----------------------------------------------------------------------------#
-# END: Helper Functions
-# ----------------------------------------------------------------------------#
-
-# ----------------------------------------------------------------------------#
 # BEGIN: Pre-launch
 # ----------------------------------------------------------------------------#
 
-# Lets get this thing started
-if !auth_token.nil?
-  gh = Ghee.access_token(auth_token)
+# Create the gihub instance
+if !config.auth_token.nil?
+  gh = Ghee.access_token(config.auth_token)
 else
-  gh = Ghee.basic_auth(username, password)
+  gh = Ghee.basic_auth(config.username, config.password)
 end
 
-if repos.include? "all"
-  repos = gh.orgs(org).repos.all.collect do |repo|
+# If the user specified "all" in the list of repos, get all the repos in the org.
+if config.repos.include? "all"
+  repos = gh.orgs(config.org).repos.all.collect do |repo|
     repo.name
   end
-  repos = repos.sort
+  config.update_repos(repos.sort)
 end
-map = Hash.new()
-numbers = Hash.new()
 
-# Pre-launch printout
-
-if verbose_mode
-  puts seperator
-  if auth_token.nil?
-    puts "Using Auth: username/password '#{username}'/'#{"*" * password.size}'"
-  else
-    puts "Using Auth: Token '#{"*" * auth_token.size}'"
-  end
-  if auto_open
-    puts "Automatically opening PRs made by;"
-  else
-    puts "Finding open PRs made by;"
-  end
-  puts "#{indent}* #{authors.join("\n#{indent}* ")}"
-  puts "In the organization '#{org}' to the following repos;"
-  puts "#{indent}* #{repos.join("\n#{indent}* ")}"
-
-  puts "Formatting;"
-  if show_link
-    puts "#{indent}Printing links for each PR."
-  else
-    puts "#{indent}Not printing links for each PR."
-  end
-  puts "#{indent}And using an indent of '#{indent}'."
-  puts seperator
-end
+# Everything is set up. Print out a status, and lets get this thing started.
+config.print
 
 # ----------------------------------------------------------------------------#
 # END: Pre-launch
@@ -247,25 +384,27 @@ end
 # BEGIN: Core Logic
 # ----------------------------------------------------------------------------#
 
+prs = PRs.new()
+
 # Go through all the repos and check for open prs
 count = 0
-repos.each do |repo|
+config.repos.each do |repo|
   # Status printout. Useful for when you're checking a lot of repos.
   count += 1
-  buffer "Getting PRs for '#{org}/#{repo}' (#{count}/#{repos.size})"
+  buffer "Getting PRs for '#{config.org}/#{repo}' (#{count}/#{config.repos.size})"
 
-  json = gh.repos(org, repo).pulls({:state => "open"}).all
+  json = gh.repos(config.org, repo).pulls({:state => "open"}).all
 
   if json.class.name != "Array"
 
     # Deal with any errors reported by the API.
     if json.message == "Bad credentials"
-      puts "Error: Invalid #{auth_token.nil? ? "username/password combo" : "auth token"} used. Exiting immediately since future requests cannot succeed"
+      puts "Error: Invalid #{config.auth_token.nil? ? "username/password combo" : "auth token"} used. Exiting immediately since future requests cannot succeed"
       exit
     elsif json.message == "Not Found"
-      puts "Error: No repo called #{repo} found in #{org}"
+      puts "Error: No repo called #{repo} found in #{config.org}"
     else
-      puts "Unknown error encountered #{json.message}, while getting Prs for #{repo} in #{org}"
+      puts "Unknown error encountered #{json.message}, while getting Prs for #{config.repo} in #{config.org}"
     end
 
   else
@@ -276,20 +415,13 @@ repos.each do |repo|
 
       # Where the author is in the list of requested authors
       # NOTE: We downcased & sorted the list of authors when we read them from the config
-      if authors.include? author
-        if !map.has_key? author
-          map[author] = Hash.new()
-        end
+      if config.authors.include? author
 
-        if !map[author].has_key? repo
-          map[author][repo] = Array.new()
-        end
-
-        number = pr['number'].to_s
+        title = pr['title']
+        number = pr['number']
         url = pr['_links']['html']['href']
 
-        map[author][repo].push({ 'title' => pr['title'], 'number' => number, 'url' => url })
-        numbers[number] = url
+        prs.add(author, repo, title, number, url)
       end
     end
 
@@ -297,55 +429,29 @@ repos.each do |repo|
 end
 buffer ""
 
-# Printout a little stats for the user so they know what was found
-puts seperator
-map.each do |author, repo|
-  puts "#{author}"
-  repo.each { |repo, commits|
-    puts "#{indent}#{commits.size} PR#{(commits.size == 1) ? "" : "s" } against #{repo}"
+# Printout a little status for the user so they know what was found
+puts config.seperator
+prs.print_status(config)
+puts config.seperator
 
-    if !quiet_mode
-      # We're not in quiet mode, print out the title
-      commits.each do |commit|
-        puts "#{indent * 2}#{commit['number']}: #{commit['title']}"
-
-        # And if they want the link printed out, do that on the next line
-        if show_link
-          puts "#{indent * 3}#{commit['url']}"
-        end
-      end
-
-    elsif show_link
-      # We're in quiet mode, but they've said they want to see the links
-      commits.each do |commit|
-        puts "#{indent * 2}#{commit['url']}"
-      end
-    end
-  }
-end
-
-noPrs = (authors - map.keys)
-puts "NOTE: #{noPrs.join(", ")} #{(noPrs.size == 1) ? "has" : "have"} no open prs"
-
-puts seperator
 puts
-puts seperator
+puts config.seperator
 
 # Either automatically open all prs, or ask the user which prs they wish to open
-if auto_open
+if config.auto_open
   puts "Automatically opening links for all found PRs."
   options = Set.new("all")
 else
-  validAuthors = (authors & map.keys).sort
-  validNumbers = numbers.keys.sort
+  validAuthors = (config.authors & prs.authors).sort
+  validNumbers = prs.numbers
   validCommands = ["all", "none"]
   allValid = (validAuthors + validNumbers + validCommands).to_set
 
-  if !quiet_mode
+  if !config.quiet_mode
     puts "You can specify PRs to open by;"
-    puts "#{indent}Author: #{validAuthors.join(", ")}"
-    puts "#{indent}PR #: #{validNumbers.join(", ")}"
-    puts "#{indent}Misc: #{validCommands.join(", ")}"
+    puts "#{config.indent}Author: #{validAuthors.join(", ")}"
+    puts "#{config.indent}PR #: #{validNumbers.join(", ")}"
+    puts "#{config.indent}Misc: #{validCommands.join(", ")}"
   end
 
   Readline.completion_proc = proc { |s| allValid.grep(/^#{Regexp.escape(s)}/) }
@@ -363,29 +469,23 @@ end
 # Open all requested PRs in the default browser
 browser = Browser.new()
 commands.each do |command|
+
   if command == "all"
-    map.each { |_,repo|
-      repo.each{ |_,commits|
-        commits.each { |commit|
-          browser.open commit['url']
-        }
-      }
-    }
+    prs.get_all_urls.each do |url|
+      browser.open url
+    end
   elsif command == "none"
     # Nothing to do here.
-  elsif map.has_key? command
-    map[command].each { |_,commits|
-      commits.each { |commit|
-        browser.open commit['url']
-      }
-    }
-  elsif numbers.has_key? command
-    browser.open numbers[command]
+  else
+    prs.urls_for(command).each do |url|
+      browser.open url
+    end
   end
+
 end
 
-if !quiet_mode
-  puts seperator
+if !config.quiet_mode
+  puts config.seperator
   puts
 end
 
